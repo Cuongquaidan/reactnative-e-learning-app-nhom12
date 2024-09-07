2ff05a0661bb66dde188a2b388512e5455be39db
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-09-07 15:47:07 +0700

    first setup and basic layout

---
+++ app/(tabs)/mycourses.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Mycourses = () => {
+    return (
+        <View>
+            <Text>Mycourses</Text>
+        </View>
+    );
+};
+
+export default Mycourses;
