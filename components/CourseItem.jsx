23c697d81a00d28c00caada3bc82c4dd52b48eb4
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-09-07 22:52:11 +0700

    Popular courses UI

---
+++ components/CourseItem.jsx
@@ -0,0 +1,126 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { Colors } from "../constants/Colors";
+
+const CourseItem = ({ course }) => {
+    const {
+        id,
+        title,
+        desc,
+        image,
+        price,
+        rating,
+        numberRating,
+        numberOfLessons,
+    } = course;
+
+    return (
+        <View
+            style={{
+                padding: 20,
+                borderRadius: 4,
+                borderWidth: 1,
+                borderColor: Colors.lightGray,
+                maxWidth: 350,
+                marginRight: 40,
+                marginVertical: 40,
+            }}
+        >
+            <Image
+                source={{ uri: image }}
+                style={{ width: "100%", height: 200 }}
+            />
+            <View
+                style={{
+                    marginTop: 40,
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontSize: 26,
+                            fontWeight: 700,
+                        }}
+                    >
+                        {title}
+                    </Text>
+                    <MaterialIcons
+                        name="bookmark-outline"
+                        size={36}
+                        color="black"
+                    />
+                </View>
+                <Text
+                    style={{
+                        fontSize: 16,
+                        color: Colors.primaryGray,
+                        marginTop: 20,
+                    }}
+                    numberOfLines={2}
+                >
+                    {desc}
+                </Text>
+                <Text
+                    style={{
+                        marginTop: 20,
+                        fontSize: 30,
+                        color: Colors.primaryBlue,
+                        fontWeight: 700,
+                    }}
+                >
+                    ${price}
+                </Text>
+                <View
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        marginVertical: 10,
+                    }}
+                >
+                    <View
+                        style={{ flexDirection: "row", alignItems: "center" }}
+                    >
+                        <FontAwesome5
+                            name="star"
+                            size={24}
+                            color={Colors.primaryYellow}
+                        />
+                        <Text> {rating}</Text>
+                        <Text
+                            style={{
+                                color: Colors.primaryGray,
+                                paddingHorizontal: 8,
+                            }}
+                        >
+                            ({numberRating})
+                        </Text>
+                    </View>
+                    <View
+                        style={{ flexDirection: "row", alignItems: "center" }}
+                    >
+                        <Text
+                            style={{
+                                color: Colors.primaryGray,
+                            }}
+                        >
+                            <Text style={{ color: Colors.primaryBlack }}>
+                                {numberOfLessons}
+                            </Text>{" "}
+                            Lessons
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+export default CourseItem;
