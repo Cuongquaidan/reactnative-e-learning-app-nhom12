865a5755711d7d006d93d6122f703b99c9456b21
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-09-10 01:17:15 +0700

    Hot topics in search page

---
+++ components/CategoryItemFullRow.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CategoryItemFullRow = () => {
+    return (
+        <View>
+            <Text>CategoryItemFullRow</Text>
+        </View>
+    );
+};
+
+export default CategoryItemFullRow;
