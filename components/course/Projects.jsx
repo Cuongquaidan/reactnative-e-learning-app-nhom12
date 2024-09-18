46d5e4a64c55cd31498e8a7eb331edb69cb37f89
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-09-18 23:55:01 +0700

    project resouces

--- components/course/Projects.jsx
+++ components/course/Projects.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { View, Text, Button, Pressable, Image, FlatList } from "react-native";
 import * as DocumentPicker from "expo-document-picker";
 import { Colors } from "../../constants/Colors";
-import AntDesign from "@expo/vector-icons/AntDesign";
+
 import ProjectItem from "../ProjectItem";
 import ViewMore from "../ViewMore";
+import AntDesign from "@expo/vector-icons/AntDesign";
 const projectStudent = [
     {
         id: 1,
@@ -23,6 +24,14 @@ const projectStudent = [
         image: "https://images.pexels.com/photos/9248674/pexels-photo-9248674.jpeg",
         name: "UX-UI clone facepen",
         author: "Neymar",
+    },
+];
+const resources = [
+    {
+        name: "content-text.docx",
+    },
+    {
+        name: "content-pdf.pdf",
     },
 ];
 const Projects = () => {
@@ -131,6 +140,62 @@ const Projects = () => {
                     perspiciatis cum error, repellendus sunt asperiores libero
                     quod!
                 </Text>
+            </View>
+            {/* Resource */}
+            <View style={{ marginTop: 30 }}>
+                <Text
+                    style={{ fontSize: 24, fontWeight: 700, marginBottom: 20 }}
+                >
+                    Resources
+                </Text>
+                <View style={{ gap: 20 }}>
+                    {resources.map((item, index) => (
+                        <View
+                            key={index}
+                            style={{
+                                flexDirection: "row",
+                                gap: 20,
+                                alignItems: "center",
+                                padding: 20,
+                                borderWidth: 1,
+                                borderColor: Colors.lightGray,
+                                borderRadius: 8,
+                                justifyContent: "space-between",
+                            }}
+                        >
+                            <View
+                                style={{
+                                    flexDirection: "row",
+                                    gap: 20,
+                                    alignItems: "center",
+                                }}
+                            >
+                                <AntDesign
+                                    name="filetext1"
+                                    size={40}
+                                    color={
+                                        (item.name.includes(".docx") &&
+                                            "blue") ||
+                                        (item.name.includes(".pdf") && "red")
+                                    }
+                                />
+                                <Text
+                                    style={{
+                                        fontSize: 20,
+                                        color: Colors.primaryGray,
+                                    }}
+                                >
+                                    {item.name}
+                                </Text>
+                            </View>
+                            <AntDesign
+                                name="download"
+                                size={24}
+                                color={Colors.primaryGray}
+                            />
+                        </View>
+                    ))}
+                </View>
             </View>
         </View>
     );
