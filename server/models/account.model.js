4067f220a6ee3767801f27804662e1df712e11c2
Hà Mạnh Cường <hamanhcuong18112004@gmail.com>
2024-11-17 17:44:52 +0700

    Add register page ui

--- server/models/account.model.js
+++ server/models/account.model.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 const AccountSchema = mongoose.Schema({
     username: { type: String, required: true },
-    avatar: { type: String, required: true },
-    dob: { type: Date, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isTeacher: { type: Boolean, required: true, default: false },
